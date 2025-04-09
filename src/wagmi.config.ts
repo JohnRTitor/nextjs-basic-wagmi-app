4481738c70cf99ba45618f21f05c6ba52236d6db
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-04-09 09:10:27 +0530

    add fallback RPC_URLs as sepolia RPC doesn't allow interaction from webpages
    Free public sepolia RPC URLs!
    https://chainlist.org/chain/11155111?testnets=true
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/wagmi.config.ts
+++ src/wagmi.config.ts
@@ -1,4 +1,4 @@
-import { createConfig, http } from "wagmi";
+import { createConfig, fallback, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 
 declare module "wagmi" {
@@ -7,10 +7,23 @@ declare module "wagmi" {
   }
 }
 
+// environment variables are defined in .env.local
+const sepoliaRpcUrl = process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL;
+const mainnetRpcUrl = process.env.NEXT_PUBLIC_MAINNET_RPC_URL;
+if (!sepoliaRpcUrl) {
+  console.error("Missing NEXT_PUBLIC_SEPOLIA_RPC_URL in environment variables.");
+}
+if (!mainnetRpcUrl) {
+  console.error("Missing NEXT_PUBLIC_MAINNET_RPC_URL in environment variables.");
+}
+
 export const config = createConfig({
   chains: [mainnet, sepolia],
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [mainnet.id]: fallback([http(mainnetRpcUrl)]),
+    [sepolia.id]: fallback([
+      http(sepoliaRpcUrl),
+      http("https://ethereum-sepolia-rpc.publicnode.com"),
+    ]),
   },
 });
